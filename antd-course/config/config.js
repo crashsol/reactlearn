6a4a6b4f8263377693ff70526f979d09b8ce48b8
crashsol <47147551@qq.com>
2018-12-02 10:16:50 +0800

    完成路由的配置及菜单的配置

--- antd-course/config/config.js
+++ antd-course/config/config.js
@@ -12,12 +12,34 @@ export default {
     ],
     /* 路由配置 */
     routes: [{
-            path: '/',
-            component: '../layout',
-            routes: [{
-                path: 'helloworld',
-                component: './HelloWorld'
-            }]
-        }
-    ]
+        path: '/',
+        component: '../layout',
+        routes: [{
+                path: '/',
+                component: 'HelloWorld'
+            },
+            {
+                path: '/helloworld',
+                component: 'HelloWorld'
+            },
+            {
+                path: '/dashboard',
+                routes: [{
+                        path: '/dashboard/analysis',
+                        component: 'Dashboard/Analysis'
+                    },
+                    {
+                        path: '/dashboard/monitor',
+                        component: 'Dashboard/Monitor'
+                    },
+                    {
+                        path: '/dashboard/workplace',
+                        component: 'Dashboard/Workplace'
+                    },
+
+
+                ]
+            }
+        ]
+    }]
 };
---
+++ antd-course/src/page/Dashboard/Monitor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Monitor extends Component {
+    render() {
+        return (
+            <div>
+                    Monitor
+            </div>
+        );
+    }
+}
+
+export default Monitor;
