308c3b706a3dba1b01950054f0b5761cf115a19b
crashsol <47147551@qq.com>
2018-12-03 21:15:30 +0800

    学习webpack,并在内容中加载index.html

--- 01/webpack.config.js
+++ 01/webpack.config.js
@@ -2,16 +2,15 @@
   向外暴露一个打包的配置对象
   因为webpck是基于Node构建的
  */
-const path =  require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin') //导入内存中自动生成index.html
-//创建一个插件实例对象
-const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.join(__dirname, './src/index.html'), //源文件
-    filename: 'index.html'
-});
+
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin'); //导入内存中自动生成index.html
 module.exports = {
     mode: 'development', //production  development
     plugins: [
-        htmlPlugin
+        new HtmlWebPackPlugin({
+            template: path.join(__dirname,'./src/index.html'), //源文件
+            filename: 'index.html'
+        })
     ]
 }
