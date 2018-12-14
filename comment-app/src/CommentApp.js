afb0633e298a82575aac17c20b90b03e114068e8
crashsol <47147551@qq.com>
2018-12-14 17:52:29 +0800

    将数据保存到本地storage

--- comment-app/src/CommentApp.js
+++ comment-app/src/CommentApp.js
@@ -8,19 +8,38 @@ export default class CommentApp extends Component {
     this.state = {
       comments: []
     }
+  }
+  /* 加载评论 */
+  componentWillMount = () => {
+    this._loadComments();
+  }
 
+  //从本地storage中加载评论
+  _loadComments = () => {
+    let comments = localStorage.getItem('comments');
+    if (comments) {
+      comments = JSON.parse(comments)
+      this.setState({
+        comments
+      })
+    }
+  }
+  // 将评论保存到本地storage
+  _saveComments = (comments) => {
+    localStorage.setItem('comments', JSON.stringify(comments))
   }
   /* 将添加评论方法通过props 传递给 CommentInput组件 ，  
   并接受CommentInput 返回的数据*/
   handleSubmitComment = (comment) => {
 
-    
     /* 将添加的的数据报错到 comments中，并重新渲染界面 */
-    this.state.comments.push(comment)
+    const comments = this.state.comments
+    comments.push(comment)
     this.setState({
-      comments: this.state.comments
+      comments: comments
     })
-
+    /* 数据保存到本地 */
+    this._saveComments(comments)
   }
   render() {
     return (
--- comment-app/src/CommentInput.js
+++ comment-app/src/CommentInput.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class CommentInput extends Component {
+  /* 限定传入props的类型 */
+  static propTypes = {
+    onSubmit: PropTypes.func
+  }
 
   constructor() {
     super()
@@ -8,6 +13,12 @@ export default class CommentInput extends Component {
       username: '',
       content: ''
     }
+  }
+  componentDidMount = () => {
+    /* 加载用户名称 */
+    this._loadUserName();
+    /* 自动聚焦到评论框 */
+    this.textarea.focus();
   }
 
   setUserName = (e) => {
@@ -20,32 +31,46 @@ export default class CommentInput extends Component {
       content: e.target.value
     })
   }
+  /* 保存用户名称到本地  */
+  _saveUserName = (username) => {
+    localStorage.setItem('username', username)
+  }
+  _loadUserName = () => {
+    const username = localStorage.getItem('username')
+    if (username) {
+      this.setState({
+        username
+      })
+    }
+  }
   /* 提交数据 */
-  handleSubmitComment = () => {   
+  handleSubmitComment = () => {
 
     // 检查数据合法性
     const { username, content } = this.state
 
-    if(!username) 
-    {
-       alert('请输入用户名')
-       return
-    }    
-    if(!content) 
-    {
-       alert('请输入评论内容')
-       return
+    if (!username) {
+      alert('请输入用户名')
+      return
     }
-   
+    if (!content) {
+      alert('请输入评论内容')
+      return
+    }
+
 
     /* 调用props传入的提交方法 */
     if (this.props.onSubmit) {
-     
+
       this.props.onSubmit({ username, content })
     }
     this.setState({
       content: ''
     })
+  }
+  /* 用户名输入框失去焦点时,将UserName 保存到本地数据库*/
+  handleUserNameBlur = (e)=>{
+    this._saveUserName(e.target.value)
   }
 
   render() {
@@ -54,13 +79,21 @@ export default class CommentInput extends Component {
         <div className='comment-field'>
           <span className='comment-field-name'>用户名：</span>
           <div className='comment-field-input'>
-            <input value={this.state.username} onChange={this.setUserName} />
+            <input
+              value={this.state.username}
+              onChange={this.setUserName}
+              onBlur={this.handleUserNameBlur}
+            />
           </div>
         </div>
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容：</span>
           <div className='comment-field-input'>
-            <textarea value={this.state.content} onChange={this.setContent} />
+            <textarea
+              ref={(textarea) => this.textarea = textarea}
+              value={this.state.content}
+              onChange={this.setContent}
+            />
           </div>
         </div>
         <div className='comment-field-button'>
