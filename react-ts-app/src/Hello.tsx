4bfb0bc2241354f42f71780311219f2074fc39f3
crashsol <47147551@qq.com>
2018-12-18 19:48:28 +0800

    添加组件State定义和方法，添加CSS样式

--- react-ts-app/src/Hello.tsx
+++ react-ts-app/src/Hello.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
+import './Hello.css'
 
-/* 定义Props 类型 */
+/* 定义传入 Props 类型 */
 export interface Props {
     name: string,
     enthusiasmLevel?: number
+}
+/* 定义当前组件的状态 */
+interface State {
+    currentEnthusiasm: number;
 }
 
 //#region 使用function 定义组件 
@@ -22,7 +27,7 @@ export interface Props {
 //#endregion
 
 //#region 使用 Class 定义组件
-class Hello extends React.Component<Props, object>
+/* class Hello extends React.Component<Props, object>
 {
     render() {
         const { name, enthusiasmLevel = 1 } = this.props
@@ -39,8 +44,46 @@ class Hello extends React.Component<Props, object>
 
         )
     }
-}
+} */
 //#ednregion
+
+class Hello extends React.Component<Props, State>{
+
+    constructor(props: Props) {
+        super(props);
+        //根据传入的数据设定当前State
+        this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 }
+    }
+
+    //更新状态
+    updateEnthusiasm(currentEnthusiasm: number): void {
+        this.setState({ currentEnthusiasm })
+    }
+
+    onIncrement = () => {
+        this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+    }
+    onDecrement = () => {
+        this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+    }
+
+    render() {
+        const { name } = this.props
+        if (this.state.currentEnthusiasm <= 0) {
+            throw new Error('You could be a little more enthusiastic. :D');
+        }
+        return (
+            <div className='hello'>
+                <div className='greeting'>
+                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+                </div>
+                <button onClick={this.onIncrement}>+</button>
+                <button onClick={this.onDecrement}>-</button>
+            </div>
+
+        )
+    }
+}
 
 
 
