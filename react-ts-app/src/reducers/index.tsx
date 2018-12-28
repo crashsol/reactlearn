1cfdd3234f1108d796093259cdcd686f8d3c2e10
crashsol <47147551@qq.com>
2018-12-28 17:47:32 +0800

    123

--- react-ts-app/src/reducers/index.tsx
+++ react-ts-app/src/reducers/index.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { EnthusiasmAction } from '../actions';
-import { StoreState } from '../types/index';
-import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from '../constants/index'
-
-export function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
-    switch (action.type) {
-        case INCREMENT_ENTHUSIASM:
-            return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 };
-        case DECREMENT_ENTHUSIASM:
-            return { ...state, enthusiasmLevel: Math.max(1, state.enthusiasmLevel - 1) };
-    }
-    return state;
-}
-=======
 /* 定义Reducer */
 import { EnthusiasmAction } from '../actions/index'
 import { StoreState } from '../types/index'
@@ -31,4 +16,3 @@ export function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreSt
     }
     return state;
 }
->>>>>>> 17dd89db877f97fd01181e95215a80b39277e50b
